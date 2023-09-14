c248769175dd2f0201b752de7287280c0982acd1
Metee <viixxivvimm@gmail.com>
2023-09-15 03:02:02 +0700

    fix: remove Vector4

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,3 @@
-export { Vector2, Vector3, Vector4, log, hash } from "./utils";
+export { Vector2, Vector3, log, hash } from "./utils";
 export { RGB, RGBA } from "./utils/RGBA";
 export * from "./natives";
